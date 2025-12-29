a547d973342bb3f1226bf406889183c7a7468462
Pruthvi-dhani <87530402+Pruthvi-dhani@users.noreply.github.com>
2025-12-29 12:07:48 +0000

    gets date counter working

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Counter />
     </div>
   );
 }
 
 export default App;
+
+function addDays(date, days) {
+  const res = new Date(date);
+  res.setDate(res.getDate() + days);
+  return res;
+}
+
+function Counter() {
+  const [step, setStep] = useState(1);
+  const [count, setCount] = useState(0);
+  const currDate = new Date("2027-06-21");
+  return (
+    <div>
+      <div>
+        <button
+          onClick={() => {
+            if (step > 1) setStep((s) => s - 1);
+          }}
+        >
+          -
+        </button>
+        Step: {step}
+        <button onClick={() => setStep((s) => s + 1)}>+</button>
+      </div>
+      <div>
+        <button onClick={() => setCount((c) => c - step)}>-</button>Count:
+        {count}
+        <button onClick={() => setCount((c) => c + step)}>+</button>
+      </div>
+      <p>Today is {addDays(currDate, count).toDateString()}</p>
+    </div>
+  );
+}
